7faec4af2ccc697127abd6d8a8a3e215c95f530b
Tomek Lipski <tomek.lipski@gmail.com>
2014-08-18 09:41:14 +0200

    refactor code to reduce number of functions + drop stripe dependency

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 var express = require('express');
-var stripe = require('stripe');
 var client = require('swagger-client');
 
 client.authorizations.add("apiKey", new client.ApiKeyAuthorization("private_token", "SamplePrivateTestKey1", "query"));
 
-function initApp (taxamo) {
-    var app = express();
-    app.get('/stripe', function index(req, res){
-        taxamo.apis.transactions.getTransaction({key: req.query.transaction_key}, function(data) {
-            var transaction = JSON.parse(data.data).transaction;
-            //you can check if transaction amounts/products make sense here
-            taxamo.apis.payments.capturePayment({key: req.query.transaction_key}, function(data) {
-                res.send("Payment accepted.");
-            }, function (error) {
-                var errorResponse = JSON.parse(error.data);
-                res.send(errorResponse);
-            });
+var app = express();
+app.get('/stripe', function index(req, res){
+    taxamo.apis.transactions.getTransaction({key: req.query.transaction_key}, function(data) {
+        var transaction = JSON.parse(data.data).transaction;
+        //you can check if transaction amounts/products make sense here
+        taxamo.apis.payments.capturePayment({key: req.query.transaction_key}, function(data) {
+            res.send("Payment accepted.");
+        }, function (error) {
+            var errorResponse = JSON.parse(error.data);
+            res.send(errorResponse);
         });
     });
-    app.use(express.static(__dirname + '/public'));
-    
-    var server = app.listen(3000, function() {
-        console.log('Listening on port %d', server.address().port);
-    });
-}
+});
+app.use(express.static(__dirname + '/public'));
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
 
 
 var taxamo = new client.SwaggerApi({
   url: 'http://localhost:3007/swagger',
   success: function() {
     if(taxamo.ready === true) {
-        initApp(taxamo);
+        var server = app.listen(3000, function() {
+            console.log('Listening on port %d', server.address().port);
+        });
     }
   }
 });
